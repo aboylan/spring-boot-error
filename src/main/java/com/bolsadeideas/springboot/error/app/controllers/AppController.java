a9647f17dd259c0e50f1ab6dfa44a5948fc8ecf0
Alex Boylan <aboylan@users.noreply.github.com>
2023-08-11 21:02:13 -0600

    Añadiendo método handler ver

--- src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -1,16 +1,34 @@
 package com.bolsadeideas.springboot.error.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.bolsadeideas.springboot.error.app.model.domain.Usuario;
+import com.bolsadeideas.springboot.error.app.services.UsuarioService;
 
 @Controller
 public class AppController {
 
+	@Autowired
+	private UsuarioService usuarioService;
+
 	@SuppressWarnings("unused")
 	@GetMapping("/index")
 	public String index() {
-		//Integer valor = 100/0;
+		// Integer valor = 100/0;
 		Integer valor = Integer.parseInt("10xaaa");
 		return "index";
 	}
+
+	@GetMapping("/ver/{id}")
+	public String ver(@PathVariable Integer id, Model model) {
+		Usuario usuario = usuarioService.obtenerPorId(id);
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Detalle usuario: ".concat(usuario.getNombre()));
+		return "ver";
+	}
+
 }
