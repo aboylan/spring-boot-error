cc3b5e4e63108d5be33e4da77e8135cb3277ab38
Alex Boylan <aboylan@users.noreply.github.com>
2023-08-11 21:11:18 -0600

    Creando clase Exception personalizada

--- src/main/java/com/bolsadeideas/springboot/error/app/controllers/ErrorHandlerController.java
+++ src/main/java/com/bolsadeideas/springboot/error/app/controllers/ErrorHandlerController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.bolsadeideas.springboot.error.app.errors.UsuarioNoEncontradoException;
+
 @ControllerAdvice
 public class ErrorHandlerController {
 
@@ -28,4 +30,12 @@ public class ErrorHandlerController {
 		return "error/numero-formato";
 	}
 	
+	@ExceptionHandler(UsuarioNoEncontradoException.class)
+	public String usuarioNoEncontrado(UsuarioNoEncontradoException ex, Model model) {
+		model.addAttribute("error", "Error: Usuario no encontrado!");
+		model.addAttribute("message", ex.getMessage());
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		model.addAttribute("timestamp", new Date());
+		return "error/usuario";
+	}
 }
